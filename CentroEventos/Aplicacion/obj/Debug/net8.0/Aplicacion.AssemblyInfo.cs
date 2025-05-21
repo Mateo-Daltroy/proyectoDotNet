82e8b7ff39cd7ebe6c3348c9901e30cc7bf4860f
matiAversa <matiasaversa1503@gmail.com>
2025-05-21 09:07:11 -0300

    asd

--- CentroEventos/Aplicacion/obj/Debug/net8.0/Aplicacion.AssemblyInfo.cs
+++ CentroEventos/Aplicacion/obj/Debug/net8.0/Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ddef90a36e9eb934b9b75dda85c03416748056d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484823a2bc31a5d9aac603815fbe1db3baead027")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CentroEventos/Consola/obj/Debug/net8.0/Consola.AssemblyInfo.cs
+++ CentroEventos/Consola/obj/Debug/net8.0/Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8aae0d55da99ed0878250ace518a2bffbe06fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484823a2bc31a5d9aac603815fbe1db3baead027")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

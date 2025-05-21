346caca1ca3851cc27e5580e7f72ff3417ed7960
matiAversa <matiasaversa1503@gmail.com>
2025-05-21 09:28:01 -0300

    merge 2

--- CentroEventos/Repositorios/obj/Debug/net8.0/Repositorios.AssemblyInfo.cs
+++ CentroEventos/Repositorios/obj/Debug/net8.0/Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5c51bb5c63ab2018482bedb46d97c6735613d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfec9205c83c447546706d96e48c552e2d4421b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
